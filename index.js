a1fa6593844f6520bad90a9bacff498bf5648243
Sharvit Kashikar <sharvitkashikar@Sharvits-MacBook-Air.local>
2025-02-02 10:05:56 +0530

    Second commit

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ app.use("/course",courseRouter)
 
 async function main() {
     try {
-        // Attempt to connect to the MongoDB database using the connection string from the .env file
+       
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to the database successfully");
 
-        // Get the port from the .env file or use the default port 3000
+       
         const port = process.env.PORT || 3000;  
         
-        // Listen on the specified port
+       
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
         });
     } catch (error) {
-        // Log any errors that occur during the connection or server setup
-        console.error("Failed to connect to the database or start the server", error);
+       
+        console.error("Failed to connect to the database or server", error);
     }
 }
 main();
